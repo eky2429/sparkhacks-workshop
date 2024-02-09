c8b687598bc9253e2857c493f91a53529422da25
Ethan Ky <132713901+eky2429@users.noreply.github.com>
2024-02-09 23:07:29 +0000

    Finished workshop!

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,40 @@
 import Head from "next/head";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+    const initialPizzas = [];
+    const initialFitlerText = "";
+    const resultFromUsedState = useState(initialPizzas);
+    const pizzasFromState = resultFromUsedState[0];
+    const setPizzas = resultFromUsedState[1];
+    const [filterText, setFilterText] = useState(initialFitlerText);
+
+    //Use effect fetches data from pizzas.js
+    useEffect(() => {
+        fetch("/api/pizzas")
+            //Converts each individual data to json
+            .then((data) => {
+                return data.json();
+            })
+            //Gets pizzas hash from pizzas.js
+            .then((response) => {
+                setPizzas(response.pizzas);
+            });
+    }, [pizzasFromState, setPizzas]);
+
+    function Pizzas() {
+        const filteredPizzas = pizzasFromState.filter(() => {
+            return pizza.name.toLowerCase().includes(filterText);
+        });
+
+        return (
+            //Takes each –pizza from pizzasFromState and ...
+            filteredPizzas.map((pizza, index) => {
+                return <div key={index}> {pizza.name}</div>;
+            })
+        );
+    }
+
     return (
         <>
             <Head>
@@ -15,7 +49,17 @@ export default function Home() {
                 />
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <main></main>
+            <main>
+                <input
+                    type="search"
+                    name="search"
+                    data-test-id="search"
+                    onChange={(event) => {
+                        setFilterText(event.target.value);
+                    }}
+                ></input>
+                <Pizzas />
+            </main>
         </>
     );
 }
